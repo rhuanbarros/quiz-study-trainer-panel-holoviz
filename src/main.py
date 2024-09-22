6ab642b31d2727f912c2a2ce76b22b8b8fabc10c
Rhuan Barros <rhuanbarros@gmail.com>
2024-09-22 16:37:33 +0000

    basic page

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 import panel as pn
+import os
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
 
-pn.extension()
+from supabase import create_client, Client
+
+url = os.environ["SUPABASE_URL"]
+key = os.environ["SUPABASE_KEY"]
+
+supabase: Client = create_client(url, key)
+
+data, count = supabase.table('get_question').select("*").execute()
+# print(data)
+question = data[1][0]
+
+# pn.extension()
+pn.extension('mathjax')
 
 
 
@@ -12,8 +27,35 @@ template = pn.template.MaterialTemplate(
 )
 # Append a layout to the main area, to demonstrate the list-like API
 template.main.append(
-    pn.Row(
-        pn.Card( pn.panel("Question 1" ), title="Question" )
+    pn.Column(
+        pn.Card( pn.pane.Markdown( question["question"] ), 
+                title="Question",
+                # hide_header=True,
+                collapsible=False,
+        ),
+        pn.Card( 
+            # pn.widgets.Button(name='A', button_type='default'),
+                pn.pane.Markdown( question["answer_a"] ), 
+                title="Option A",
+                # hide_header=True,
+                collapsible=False,
+        ),
+        pn.Card( pn.pane.Markdown( question["answer_b"] ), 
+                title="Option B",
+                # hide_header=True,
+                collapsible=False,
+        ),
+        pn.Card( pn.pane.Markdown( question["answer_c"] ), 
+                title="Option C",
+                # hide_header=True,
+                collapsible=False,
+        ),
+        pn.Card( pn.pane.Markdown( question["answer_d"] ), 
+                title="Option D",
+                # hide_header=True,
+                collapsible=False,
+        ),
+        pn.widgets.Button(name='I dont know', button_type='primary')
     )
 )
 
